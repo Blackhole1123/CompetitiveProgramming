f03c54cdc7325e66a64f502fbb21ead227701a54
Rishaan Desai <rishaandesai41@gmail.com>
2022-06-01 19:52:48 -0700

    Shortest Word

--- CodeWars/7 kyu/ShortestWord.py
+++ CodeWars/7 kyu/ShortestWord.py
@@ -1,9 +1,9 @@
 def find_short(s):
     s = s.split(' ')
-    shortlen = 0
+    short = 0
     for i in range(len(s)):
         if i == 0:
-            shortlen = len(s[i])
-        elif (i != 0 and len(s[i]) < shortlen):
-            shortlen = len(s[i])
-    return shortlen
+            short = len(s[i])
+        elif (i != 0 and len(s[i]) < short):
+            short = len(s[i])
+    return short
