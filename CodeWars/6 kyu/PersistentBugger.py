6501fcf38ee71c0ba1877b2aba637b2a45b30115
Rishaan Desai <rishaandesai@gmail.com>
2022-06-01 19:25:59 -0700

    Persistent Bugger

---
+++ CodeWars/6 kyu/PersistentBugger.py
@@ -0,0 +1,23 @@
+from audioop import mul
+from posixpath import split
+from turtle import clear
+
+
+def splitint(n):
+    return [int(i) for i in str(n)]
+
+def multiply(n):
+    c = 1
+    for i in n:
+        c *= i
+    return c
+
+def persistence(n):
+    if len(str(n)) == 1:
+        return 0
+    else:
+        count = 0
+        while len(str(n)) != 1:
+            n = multiply(splitint(n))
+            count += 1
+        return count
